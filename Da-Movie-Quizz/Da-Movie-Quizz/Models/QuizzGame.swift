6654a2e7e6080eeb8ffc2bf31dedfb4faa15aa10
CostardRouge <mncorp971@gmail.com>
2015-09-14 22:02:19 +0200

    main model added

---
+++ Da-Movie-Quizz/Da-Movie-Quizz/Models/QuizzGame.swift
@@ -0,0 +1,38 @@
+//
+//  QuizzGame.swift
+//  Da-Movie-Quizz
+//
+//  Created by Steeve Pommier on 14/09/15.
+//  Copyright (c) 2015 CostardRouge. All rights reserved.
+//
+
+import UIKit
+
+enum eQuizzTimeMode {
+    case Unlimited, Limited
+}
+
+enum eMoviesNationality {
+    case Unlimited, Limited
+}
+
+enum eActorsGender {
+    case All, Male, Female
+}
+
+class QuizzGame: NSObject {
+    
+    var roundCount:Int = 0
+    var scoreCount:Int = 0
+    
+    var timePlayed:NSDate = NSDate()
+    
+    var timeMode:eQuizzTimeMode = .Limited
+    var actorsGender:eActorsGender = .All
+    
+    var playerUsername:String = "DMQ"
+    
+    func currentOrder() {
+    
+    }
+}
